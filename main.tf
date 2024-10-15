5987e3f2d15818108f5fbfc8f5041300d2db7832
mahir-makwana <157219128+mahir-makwana@users.noreply.github.com>
2024-10-16 00:19:58 +0530

    Adding tf confing for  s3

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.69.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "demo-bucket" {
+  bucket = "tf-cloud-${terraform.workspace}-${random_id.rand_id.hex}"
+}
